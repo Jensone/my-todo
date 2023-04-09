762a4e181db0bd1e6a84e75e9df4a91a6cca75ea
Jensone <hello@jensone.com>
2023-04-09 18:08:28 +0200

    🔐 Secure Home route

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,10 +9,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function home(): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    }
+
+    #[Route('/', name: 'app_index')]
+    public function index(): Response
+    {
+        return $this->redirectToRoute('app_home');
     }
 }
